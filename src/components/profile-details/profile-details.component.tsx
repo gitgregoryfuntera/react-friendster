3b0e2a5469187be885fc8ed0506de14ea3eefe40
gitgregoryfuntera <gregory.funtera@gmail.com>
2022-04-30 13:28:51 +0800

    profile details

--- src/components/profile-details/profile-details.component.tsx
+++ src/components/profile-details/profile-details.component.tsx
@@ -4,7 +4,23 @@ import classes from "./profile-details.module.scss";
 const ProfileDetails = () => {
   return (
     <div className={classes["profileDetailsContainer"]}>
-      ProfileDetails Works!
+      <div className={classes["detailsContainer"]}>
+        <div className={classes["spacer"]}></div>
+        <div className={classes["details"]}>
+          <div>
+            <h4>Name</h4>
+            <p>John Doe</p>
+          </div>
+          <div>
+            <h4>Name</h4>
+            <p>John Doe</p>
+          </div>
+          <div>
+            <h4>Name</h4>
+            <p>John Doe</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
