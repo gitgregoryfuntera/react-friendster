27761d087ed9bae3cb1f8ea8d277059d22391c85
gitgregoryfuntera <gregory.funtera@gmail.com>
2022-05-08 08:42:08 +0800

    added testimonial component

---
+++ src/components/testimonials/testimonials.component.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Testimonial from '../testimonial/testimonial.component';
+import './testimonials.module.scss';
+
+const Testimonials = () => {
+    return <div>
+        <div>
+            <h4>Testimonial</h4>
+        </div>
+        <div>
+            <Testimonial/>
+        </div>
+    </div>
+}
+
+export default Testimonials;
--- src/pages/profile/profile-page.component.tsx
+++ src/pages/profile/profile-page.component.tsx
@@ -3,18 +3,21 @@ import ProfileDetails from "../../components/profile-details/profile-details.com
 import ProfileHeroBanner from "../../components/profile-hero-banner/profile-hero-banner.component";
 import ProfilePicture from "../../components/profile-picture/profile-picture.component";
 import ProfileShoutOut from "../../components/profile-shout-out/profile-shout-out.component";
+import Testimonials from "../../components/testimonials/testimonials.component";
 import classes from "./profile-page.module.scss";
 
 const ProfilePage = () => {
   return (
-    <div>
+    <div className="pageLayout">
       <div className={classes["profileHeaderContainer"]}>
         <ProfileHeroBanner />
         <ProfilePicture />
         <ProfileShoutOut />
         <ProfileDetails />
       </div>
-      Profile Page
+      <div>
+        <Testimonials />
+      </div>
     </div>
   );
 };
